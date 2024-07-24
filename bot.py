7efa3233072a67a6746d19039039f53abddd403a
zer0reaction <boltgopnik@gmail.com>
2024-07-24 13:02:45 +0300

    Inline query test

--- bot.py
+++ bot.py
@@ -1,7 +1,9 @@
 from pyrogram import *
+from pyrogram.types import (InlineQueryResultArticle, InputTextMessageContent, InlineKeyboardMarkup, InlineKeyboardButton)
 import creds
 
 
+# Specified in creds.py file
 vergil = Client(
     name=creds.bot_name,
     api_id=creds.api_id,
@@ -10,14 +12,25 @@ vergil = Client(
 )
 
 
+# Handling incoming messages
 @vergil.on_message()
 async def on_message(client, message):
-    await message.forward(message.from_user.id)
+    ...
+
+
+# Handling inline queries (e.g. @berrieddelight_bot hi)
+@vergil.on_inline_query()
+async def on_inline_query(client, inline_query):
+    await vergil.answer_inline_query(
+        inline_query_id=inline_query.id,
+        results=[
+            InlineQueryResultArticle("Title", InputTextMessageContent("Message content"))
+        ]
+    )
 
 
 async def main():
     async with vergil:
-        # With starts and ends bot
         ...
 
 
